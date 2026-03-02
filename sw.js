19a841bca096bae72fbd8c9c6a924d7069f1220f
Tatsuya-M-1259 <sstop1975@gmail.com>
2026-03-02 10:14:56 +0900

    Update sw.js
    Signed-off-by: Tatsuya-M-1259 <sstop1975@gmail.com>

--- sw.js
+++ sw.js
@@ -1,7 +1,5 @@
 // sw.js
-
-// バージョンを更新することでブラウザに新着を知らせ、キャッシュを刷新します
-const CACHE_NAME = 'houseplant-care-v2026-03-02-v4'; 
+const CACHE_NAME = 'houseplant-care-v5'; // バージョンをv5に更新
 
 const ASSETS_TO_CACHE = [
   './',
@@ -14,41 +12,27 @@ const ASSETS_TO_CACHE = [
   './icon-512x512.png'
 ];
 
-// インストール時にアセットをキャッシュ
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS_TO_CACHE);
-    }).then(() => {
-      // 新しいサービスワーカーをすぐに有効化
-      return self.skipWaiting();
-    })
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS_TO_CACHE))
+    .then(() => self.skipWaiting())
   );
 });
 
-// アクティベート時に古いキャッシュを自動削除
 self.addEventListener('activate', (event) => {
   event.waitUntil(
     caches.keys().then((cacheNames) => {
       return Promise.all(
         cacheNames.map((cacheName) => {
-          if (cacheName !== CACHE_NAME) {
-            console.log('Deleting old cache:', cacheName);
-            return caches.delete(cacheName);
-          }
+          if (cacheName !== CACHE_NAME) return caches.delete(cacheName);
         })
       );
-    }).then(() => {
-      // 全クライアントを新しいSWに即座に切り替え
-      return self.clients.claim();
-    })
+    }).then(() => self.clients.claim())
   );
 });
 
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request).then((response) => response || fetch(event.request))
   );
 });
